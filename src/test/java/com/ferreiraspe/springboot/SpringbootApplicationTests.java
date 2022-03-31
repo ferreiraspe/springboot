7e219fdfa1cb379436f9a9d8433f0fb35fdc10d6
Edson Ferreira <ferreiraspe@gmail.com>
2022-03-31 02:03:18 -0300

    Created Project

---
+++ src/test/java/com/ferreiraspe/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreiraspe.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
